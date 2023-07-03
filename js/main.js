d758a52bada10ea5a44eece9fe1c4ecbdfde76ea
FrancescoGuggino <landongu@gmail.com>
2023-07-03 15:04:26 +0200

    prompt e console

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@ let distance = prompt ("Inserisci i km da percorrere:");
 let pricekm = 0.21;
 
 
-//elaborazione calcoli
+//sconti applicabili e prezzi
+let minorsale = distance * pricekm  * 20 / 100;
+let seniorsale = distance * pricekm * 40 / 100;
+let normalprice = distance * pricekm;
+let minor = normalprice - minorsale;
+let senior = normalprice - seniorsale;
+
+
+
+// calcoli variabile età e stampa
+if (age < 18) {
+    console.log (`Il costo del tuo biglietto è:
+    ${minor}`);
+  } else if (age > 60) {  
+    console.log (`Il costo del tuo biglietto è:
+    ${senior}`);
+  } else {
+    console.log (`Il costo del tuo biglietto è:
+    ${normalprice}`);
+  }
