7ee7ffaab898e681ffcc8cde7a7ba1052a8a1443
FrancescoGuggino <landongu@gmail.com>
2023-07-03 14:04:23 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// chiedere informazioni al cliente
+let name = prompt ("inserisci il nome e cognome:");
+let age = prompt ("Inserisci l'età:");
+let distance = prompt ("Inserisci i km da percorrere:");
+let pricekm = 0.21;
+
+
+//elaborazione calcoli
